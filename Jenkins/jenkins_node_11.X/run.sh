1b182e2164365244c0a187063f1b37b005dab603
Ekansh <singhekansh22@gmail.com>
2019-04-27 15:10:16 +0530

    bug fix

--- Jenkins/jenkins_node_11.X/run.sh
+++ Jenkins/jenkins_node_11.X/run.sh
@@ -5,5 +5,5 @@ mkdir jenkins
 chown 1000 jenkins
 cd -
 docker build -t jenkins .
-docker run -d --name=jenkins -p 8080:8080 -p 50000:50000 -v $PWD/jenkins:/var/jenkins_home jenkins
+docker run -d --name=jenkins -p 8080:8080 -p 50000:50000 -v ~/jenkins:/var/jenkins_home jenkins
 echo "wait for container to start. run 'docker logs jenkins' to get admin password "
